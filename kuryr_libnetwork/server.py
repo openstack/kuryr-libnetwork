6f6ff116451e5287e1181342c9db1208de312dd7
Antoni Segura Puimedon <antonisp@celebdor.com>
2016-09-13 18:55:00 +0200

    container: Build the container from tree not git
    Up until this patch, the Docker container for the Kuryr libnetwork
    daemon was part of contrib and was being built by checking out the
    latest master from github. That is not very helpful when developers
    want to build the container to run their latest changes.
    
    With this change, we move the Docker building to the root of the repo so
    that it can use the current state of the repo to build and update the
    README.rst to contain the information relative to building and using the
    container.
    
    Change-Id: Ie9032d62c73d873a29cabe84c64a19df13fdb749
    Signed-off-by: Antoni Segura Puimedon <antonisp@celebdor.com>

--- kuryr_libnetwork/server.py
+++ kuryr_libnetwork/server.py
@@ -9,7 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
+import os
 import sys
 
 from oslo_log import log
@@ -20,16 +20,24 @@ from kuryr_libnetwork import config
 from kuryr_libnetwork import controllers
 
 
-def start():
+def configure_app():
     config.init(sys.argv[1:])
     log.setup(config.CONF, 'kuryr')
     controllers.neutron_client()
     controllers.check_for_neutron_ext_support()
     controllers.check_for_neutron_ext_tag()
 
+
+def start():
+    configure_app()
     kuryr_uri = parse.urlparse(config.CONF.kuryr_uri)
     app.run(kuryr_uri.hostname, kuryr_uri.port)
 
 
 if __name__ == '__main__':
     start()
+elif 'UWSGI_ORIGINAL_PROC_NAME' in os.environ:
+    # The module is being loaded by uWSGI to get the Flask app running under
+    # it. This allows Neutron to be set, since uWSGI does not run 'start',
+    # which would trigger the embedded Flask wsgi development server.
+    configure_app()
