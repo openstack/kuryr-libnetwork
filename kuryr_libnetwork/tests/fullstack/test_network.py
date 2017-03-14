db67818bd88fd48696cfadb37c9d9c227619354f
Dongcan Ye <hellochosen@gmail.com>
2017-03-14 16:59:02 +0800

    Add fullstack test for IPv6 subnet
    Add create and delete dualstack network with kuryr-libnetwork driver.
    
    Depends-On: I0721f5896710e221ae028c62e69768b51014d0af
    Change-Id: I70deae42cc060a143e68a3853b217ec30a6ba31a
    Partially-Implements: blueprint ipv6-subnet

--- kuryr_libnetwork/tests/fullstack/test_network.py
+++ kuryr_libnetwork/tests/fullstack/test_network.py
@@ -12,6 +12,7 @@
 
 
 from kuryr.lib import utils as lib_utils
+from kuryr_libnetwork import constants
 from kuryr_libnetwork.tests.fullstack import kuryr_base
 from kuryr_libnetwork import utils
 
@@ -56,6 +57,69 @@ class NetworkTest(kuryr_base.KuryrBaseTest):
         network = self.neutron_client.list_networks(
             tags=utils.make_net_tags(net_id))
         self.assertEqual(0, len(network['networks']))
+
+    def test_create_delete_dualstack_network_with_kuryr_driver(self):
+        """Create and Delete docker dual-stack network with Kuryr
+
+           This method creates a docker network with Kuryr driver
+           and tests it was created in Neutron.
+           It then deletes the docker network and tests that it was
+           deleted from Neutron.
+        """
+        fake_ipam = {
+            "Driver": "kuryr",
+            "Options": {},
+            "Config": [
+                {
+                    "Subnet": "10.4.0.0/16",
+                    "IPRange": "10.4.0.0/24",
+                    "Gateway": "10.4.0.1"
+                },
+                {
+                    "Subnet": "2001:db8:a0b:12f0::/64",
+                    "IPRange": "2001:db8:a0b:12f0::/64",
+                    "Gateway": "2001:db8:a0b:12f0::1"
+                }
+            ]
+        }
+        net_name = lib_utils.get_random_string(8)
+        res = self.docker_client.create_network(name=net_name,
+                                                driver='kuryr',
+                                                enable_ipv6=True,
+                                                ipam=fake_ipam)
+        net_id = res['Id']
+        try:
+            network = self.neutron_client.list_networks(
+                tags=utils.make_net_tags(net_id))
+        except Exception as e:
+            self.docker_client.remove_network(net_id)
+            message = ("Failed to list neutron networks: %s")
+            self.fail(message % e.args[0])
+        self.assertEqual(1, len(network['networks']))
+        neutron_netid = network['networks'][0]['id']
+        subnets = self.neutron_client.list_subnets(
+            network_id=neutron_netid)
+        self.assertEqual(2, len(subnets['subnets']))
+        subnet_version = []
+        subnet_name = []
+        for subnet in subnets['subnets']:
+            subnet_version.append(subnet['ip_version'])
+            subnet_name.append(subnet['name'])
+        self.assertIn(4, subnet_version)
+        self.assertIn(6, subnet_version)
+        self.assertIn(
+            constants.SUBNET_NAME_PREFIX + '10.4.0.0/24',
+            subnet_name)
+        self.assertIn(
+            constants.SUBNET_NAME_PREFIX + '2001:db8:a0b:12f0::/64',
+            subnet_name)
+        self.docker_client.remove_network(net_id)
+        network = self.neutron_client.list_networks(
+            tags=utils.make_net_tags(net_id))
+        self.assertEqual(0, len(network['networks']))
+        subnets = self.neutron_client.list_subnets(
+            network_id=neutron_netid)
+        self.assertEqual(0, len(subnets['subnets']))
 
     def test_create_delete_network_without_kuryr_driver(self):
         """Create and Delete docker network without Kuryr
