17271c508cb7edf09f4089f206f8f45afb0e86f0
Hongbin Lu <hongbin.lu@huawei.com>
2017-08-31 18:43:06 -0400

    Change config file directory in gate
    Change kuryr config file dir from /etc/kuryr to /etc/kuryr-libnetwork.
    This is because the gate expects the location as /etc/<project-name>.
    If the location is wrong, the config files won't be copied over
    at the end of the gate.
    
    Change-Id: Icce77b8cfb2885e7e8a625913773dbc97d223876

--- kuryr_libnetwork/tests/contrib/gate_hook.sh
+++ kuryr_libnetwork/tests/contrib/gate_hook.sh
@@ -8,5 +8,6 @@ GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
 
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin devstack-plugin-container https://github.com/openstack/devstack-plugin-container"
+export DEVSTACK_LOCAL_CONFIG+=$'\n'"KURYR_CONFIG_DIR=/etc/kuryr-libnetwork"
 
 $BASE/new/devstack-gate/devstack-vm-gate.sh
