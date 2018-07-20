5b31188bd2a0e704de0eb05607e6d1055579cff4
Chuck Short <chucks@redhat.com>
2018-07-20 08:27:51 -0400

    Switch to using stestr
    According to Openstack summit session [1] stestr
    is maintained project to which all Openstack projects should migrate.
    Let's switch it then.
    
    [1] https://etherpad.openstack.org/p/YVR-python-pti
    
    Change-Id: I93718ec3a72ac560aca4c9c84d1ac6078adbe090
    Signed-off-by: Chuck Short <chucks@redhat.com>

--- tools/ostestr_compat_shim.sh
+++ tools/ostestr_compat_shim.sh
@@ -2,7 +2,7 @@
 
 # preserve old behavior of using an arg as a regex when '--' is not present
 case $@ in
-    (*--*) ostestr $@;;
-    ('') ostestr;;
-    (*) ostestr --regex "$@"
+    (*--*) stestr run $@;;
+    ('') stestr run;;
+    (*) stestr run --regex "$@"
 esac
