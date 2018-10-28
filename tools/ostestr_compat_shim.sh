755e7c0c48b31581add281fa050d2bf975e424ea
Hongbin Lu <hongbin034@gmail.com>
2018-10-28 21:19:58 +0000

    Fix an issue on running subset of tests
    It broke after the switching to stestr [1] since stestr doesn't
    have the --regex arguments.
    
    [1] https://review.openstack.org/#/c/571471/
    
    Change-Id: Ied13d3c370bba2a18cd54edae0dc80050e87f07e
    Closes-Bug: #1800387

--- tools/ostestr_compat_shim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# preserve old behavior of using an arg as a regex when '--' is not present
-case $@ in
-    (*--*) stestr run $@;;
-    ('') stestr run;;
-    (*) stestr run --regex "$@"
-esac
